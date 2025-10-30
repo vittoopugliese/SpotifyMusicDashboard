5ae971812f3be52bde87e03811c856a62b56072e
Vittorio <vittoopugliese@gmail.com>
2025-10-29 21:18:53 -0300

    new colors, dark and light mode toggler

---
+++ components/ui/mode-toggle.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // eslint-disable-next-line react-hooks/set-state-in-effect
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) {
+    return (
+      <Button variant="outline" size="icon" disabled>
+        <Sun className="opacity-50" />
+      </Button>
+    );
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon" onClick={() => setTheme(theme === "light" ? "dark" : "light")} >
+          {theme === "light" ? <Sun color="black" /> : <Moon />}
+        </Button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
+}
